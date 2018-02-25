6e0b236fe93c2beeec587589123529dce18051e2
mario_chan15 <mario.luiz.miranda@gmail.com>
2018-02-25 15:28:43 -0300

    Alguns logs

--- controllers/verification.js
+++ controllers/verification.js
@@ -4,9 +4,13 @@ module.exports = (req, res) => {
     const hubMode = req.query['hub.mode'];
     const verifyTokenMatches = (req.query['hub.verify_token'] === 'Teste_Bot');
 
+    console.log('begin validation');
+
     if (hubMode && verifyTokenMatches) {
+        console.log('200');
         res.status(200).send(hubChallenge);
     } else {
+        console.log('403');
         res.status(403).end();
     }
 };
